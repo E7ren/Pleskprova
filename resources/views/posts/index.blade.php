a3c25e0d14d365d928e12f46bb391b16480c50b3
Efren <efrenmonsonis28@gmail.com>
2026-02-10 09:50:23 +0100

    prova3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="col-md-12 shadow-sm p-3 mb-5 bg-body rounded">
             @forelse ($posts as $post)
-                <div class="card mb-3">
+                <div class="card mb-3 shadow-sm p-3 mb-5 bg-body rounded border-1 border-secondary">
                     <div class="card-body">
                         <h5 class="card-title">{{ $post->titulo }} @if($post->usuario)({{ $post->usuario->login }})@endif</h5>
                         <div class="d-flex justify-content-between align-items-center">
